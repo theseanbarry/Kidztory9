d66bf9e2ab533b985900d8e8ae5a772411269f1d
Sean Barry <Sean@Seans-MacBook-Air.local>
2018-03-15 21:57:59 +0000

    \

---
+++ Kidztory 9/PageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PageViewController.swift
+//  Kidztory 9
+//
+//  Created by Sean Barry on 11/1/2018.
+//  Copyright © 2018 Sean Barry. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+import SpriteKit
+
+class PageViewController: UIViewController {
+    
+    
+    /*
+    @IBOutlet weak var backgroundImageView: UIImageView! {
+    }
+    @IBAction func arrowLeftButton(_ sender: Any) {
+       forward = true}
+    
+    @IBAction func arrowRightButton(_ sender: Any) {
+        forward = false)
+    } */
+    
+    
+    
+}
