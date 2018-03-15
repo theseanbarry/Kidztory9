88727985803c7d6cf408ec9ec6cc94f038309ce8
Sean Barry <Sean@Seans-MacBook-Air.local>
2018-03-15 21:54:34 +0000

    1

--- Kidztory 9/ViewController.swift
+++ Kidztory 9/ViewController.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import AVFoundation
+import SpriteKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var openingAnimationOutlet: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        playVOSound(99)
+        /* var openingAnimationList = [UIImage]()
+        for i in 0...3  {
+            let imageName = "logo\(i)"
+            if let image = UIImage(named: imageName) {
+                openingAnimationList.append(image) } else {return}
+        }
+        self.openingAnimationOutlet.animationImages = openingAnimationList
+        self.openingAnimationOutlet.animationDuration = 2
+        self.openingAnimationOutlet.startAnimating()
+ */
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()) {
+            self.performSegue(withIdentifier: "openingAnimationToTitleSegue", sender: nil)
+            playVOSound(0)
+        }
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func readToMeButton(_ sender: Any) {
+        readToMe = true
+        performSegue(withIdentifier: "readToMeSegue", sender: nil)
+    }
+    @IBAction func myselfButton(_ sender: Any) {
+        readToMe = false
+        performSegue(withIdentifier: "readToMeSegue", sender: nil)
+    }
+    @IBAction func moreButton(_ sender: Any) {
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +49,4 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
